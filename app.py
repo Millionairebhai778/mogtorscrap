6c05e940fe33fb9edd141b76aa257fbb3fca6ed1
Millionairebhai776 <105584053+Millionairebhai776@users.noreply.github.com>
2022-12-26 18:26:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://Millionairebhai776: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@github.com/Millionairebhai776/reponame ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://Millionairebhai776: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@github.com/Millionairebhai776/fwautof ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
