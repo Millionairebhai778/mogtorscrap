d935e1c2df0aa1176a238881a5272a3de3442ad2
Akhil Chandran <119779525+Millionairebhai778@users.noreply.github.com>
2022-12-30 12:58:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://Millionairebhai776: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@github.com/Millionairebhai776/fwautof ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://Millionairebhai778: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@github.com/Millionairebhai778/fwautof ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
